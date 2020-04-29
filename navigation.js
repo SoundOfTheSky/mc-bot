4617e7b2ecab34ffb29a16460398e7a125e33af9
SoundOfTheSky <tkachiov.2000@gmail.com>
2020-04-29 17:38:48 +0300

    retard update

--- navigation.js
+++ navigation.js
@@ -1,203 +1,219 @@
 const aStar = require('./aStar');
 const vec3 = require('vec3');
-const EPSILON = 0.000001;
-const jumpHeights = [0,1,1,1,1,-1];
-const blocksToAvoid=[];
-let blocks={};
-function blockAt(bot,p) {
-  let block=blocks[p.x+'_'+p.y+'_'+p.z];
-  if(!block) block=bot.blockAt(p);
-  return block;
-}
-function Node(point,action) {
-  this.point=point;
-  this.action=action;
-  this.toString = ()=>{
-    return this.point.x+'_'+this.point.y+'_'+this.point.z;
-  }
+function Node(pos,yaw,forward,right,jump,onGround=true,sprint=true,vel=vec3(0,0,0)) {
+  this.point=pos;
+  this.yaw=yaw;
+  this.forward=forward;
+  this.right=right;
+  this.jump=jump;
+  this.onGround=onGround;
+  this.sprint=sprint;
+  this.vel=vel;
+  this.toString = ()=>this.point.x+'_'+this.point.y+'_'+this.point.z;
 }
 const vecMid = v=>v.floored().offset(.5,0,.5);
-function moveToPoint(bot,p,sprint=true, jump=false, longjump=false,swim=false) {
-  bot.entity.position=vecMid(bot.entity.position);
-  return new Promise((r,j)=>{
-    const interval = setInterval(()=>{
-      bot.lookAt(vec3(p.x+0.5,bot.entity.position.y+1.9,p.z+0.5));
-      if(longjump&&bot.entity.position.distanceTo(vecMid(bot.entity.position.floored()))>0.4) bot.setControlState('jump', true);
-      if(jump) bot.setControlState('jump', true);
-      if(swim) {
-        bot.entity.position.x +=p.x > bot.entity.position.x?0.1:-0.1;
-        bot.entity.position.y +=p.y > bot.entity.position.y?0.1:-0.1;
-        bot.entity.position.z +=p.x > bot.entity.position.z?0.1:-0.1;
-      }
-      if(bot.entity.position.distanceTo(vecMid(p))<0.2) {
-        console.log('ok');
-        clearInterval(interval);
-        bot.setControlState('forward', false);
-        bot.setControlState('sprint', false);
-        bot.setControlState('jump', false);
-        r();
-      }
-    },50);
-    bot.setControlState('forward', true);
-    bot.setControlState('sprint', true);
-  });
-}
-const navigate = async (bot,coords, options={})=>{
-  bot.physics.jumpSpeed=10;
-  const end = coords.floored();
-  const results = aStar({
-    start: new Node(bot.entity.position.floored(), 'walk'),
-    isEnd: node=>node.point.distanceTo(end) <= 0.1,
-    neighbor: node=>getNeighbors(node,bot),
-    distance: (a,b)=>a.point.distanceTo(b.point),
-    heuristic: node=>node.point.distanceTo(end)
-  });
-  if(results.status==='success') {
-    for(const node of results.path) {
-      console.log(node);
-      await moveToPoint(bot,node.point,false,node.action==='jump', node.action==='longjump', node.action==='swim');
-    }
-    console.log('FINISH')
-  }
-  //results.path = results.path.map(nodeCenterOffset)
-  return results
-}
 const isWater = b => b.type===8||b.type===9;
-function getBoundingBox (bot,pos) {
-  return {
+const EPSILON = 0.000001;
+class Navigator {
+  constructor(bot) {
+    this.bot=bot;
+  }
+  blocksToAvoid=[];
+  blocks={};
+  blockAt(p) {
+    let block=this.blocks[p.x+'_'+p.y+'_'+p.z];
+    if(!block) block=this.bot.blockAt(p);
+    return block;
+  }
+  moveToPoint(p,sprint=true, jump=false, longjump=false,swim=false) {
+    this.bot.entity.position=vecMid(this.bot.entity.position);
+    return new Promise((r,j)=>{
+      const interval = setInterval(()=>{
+        // FIX LOOK
+        this.bot.lookAt(vec3(p.x+0.5,bot.entity.position.y+1.9,p.z+0.5));
+        if(longjump&&this.bot.entity.position.distanceTo(vecMid(bot.entity.position.floored()))>0.4) this.bot.setControlState('jump', true);
+        if(jump) this.bot.setControlState('jump', true);
+        if(swim) {
+          this.bot.entity.position.x +=p.x > this.bot.entity.position.x?0.1:-0.1;
+          this.bot.entity.position.y +=p.y > this.bot.entity.position.y?0.1:-0.1;
+          this.bot.entity.position.z +=p.x > this.bot.entity.position.z?0.1:-0.1;
+        }
+        if(this.bot.entity.position.distanceTo(vecMid(p))<0.2) {
+          clearInterval(interval);
+          this.bot.setControlState('forward', false);
+          this.bot.setControlState('sprint', false);
+          this.bot.setControlState('jump', false);
+          r();
+        }
+      },50);
+      this.bot.setControlState('forward', true);
+      this.bot.setControlState('sprint', true);
+    });
+  }
+  navigate = async (p, options={})=>{
+    this.bot.physics.jumpSpeed=10;
+    const end = p.floored();
+    const results = aStar({
+      start: new Node(this.bot.entity.position.floored(), 'walk'),
+      isEnd: node=>node.point.distanceTo(end) <= 0.1,
+      neighbor: node=>this.getNeighbors(node,bot),
+      distance: (a,b)=>a.point.distanceTo(b.point),
+      heuristic: node=>node.point.distanceTo(end)
+    });
+    if(results.status==='success') {
+      for(const node of results.path) {
+        await this.moveToPoint(bot,node.point,false,node.action==='jump', node.action==='longjump', node.action==='swim');
+      }
+    }
+    console.log(results.status);
+  }
+  getBoundingBox = pos=>({
     min: new Vec3(
-      pos.x - bot.physics.playerApothem,
-      pos.y,
-      pos.z - bot.physics.playerApothem
+        pos.x - this.bot.physics.playerApothem,
+        pos.y,
+        pos.z - this.bot.physics.playerApothem
     ).floor(),
     max: new Vec3(
-      pos.x + bot.physics.playerApothem,
-      pos.y + bot.physics.playerHeight,
-      pos.z + bot.physics.playerApothem
+        pos.x + this.bot.physics.playerApothem,
+        pos.y + this.bot.physics.playerHeight,
+        pos.z + this.bot.physics.playerApothem
     ).floor()
-  }
-}
-function collisionInRange (bot,boundingBoxMin, boundingBoxMax) {
-  const cursor = new Vec3(0, 0, 0)
-  let block
-  for (cursor.x = boundingBoxMin.x; cursor.x <= boundingBoxMax.x; cursor.x++) {
-    for (cursor.y = boundingBoxMin.y; cursor.y <= boundingBoxMax.y; cursor.y++) {
-      for (cursor.z = boundingBoxMin.z; cursor.z <= boundingBoxMax.z; cursor.z++) {
-        block = blockAt(bot,cursor);
-        if (block && block.boundingBox === 'block') return true
+  });
+  collisionInRange (boundingBoxMin, boundingBoxMax) {
+    const cursor = new Vec3(0, 0, 0)
+    let block
+    for (cursor.x = boundingBoxMin.x; cursor.x <= boundingBoxMax.x; cursor.x++) {
+      for (cursor.y = boundingBoxMin.y; cursor.y <= boundingBoxMax.y; cursor.y++) {
+        for (cursor.z = boundingBoxMin.z; cursor.z <= boundingBoxMax.z; cursor.z++) {
+          block = this.blockAt(cursor);
+          if (block && block.boundingBox === 'block') return true
+        }
       }
     }
+    return false
   }
-  return false
-}
-function emulateLongjump(bot,yaw) {
-  const pos=bot.entity.position.clone();
-  const vel = vec3(0,10,0);
-  while(true) {
-    const acceleration=vec3(0,0,0)
-    vel.x += bot.physics.walkingAcceleration * -Math.sin(yaw)*bot.physics.sprintSpeed;
-    vel.z += bot.physics.walkingAcceleration * -Math.cos(yaw)*bot.physics.sprintSpeed;
-    const oldGroundSpeedSquared = vel.x * vel.x + vel.z * vel.z;
-    if (oldGroundSpeedSquared < EPSILON) {
-      vel.x = 0
-      vel.z = 0
-    } else {
-      const oldGroundSpeed = Math.sqrt(oldGroundSpeedSquared)
-      let groundFriction = bot.physics.groundFriction * bot.physics.walkingAcceleration*0.05;
-      acceleration.x -= vel.x / oldGroundSpeed * groundFriction;
-      acceleration.z -= vel.z / oldGroundSpeed * groundFriction;
-    }
-    vel.add(acceleration.scaled(0.05));
-  }
-}
-function emulatePhysics(bot,yaw,sprint=true,jump=true,pos,onGround,vel) {
-  let t=0;
-  vel=vel?vel:vec3(0,0,0);
-  onGround=onGround?onGround:bot.entity.onGround;
-  pos=pos?pos:bot.entity.position.clone();
-  yaw=yaw?yaw:bot.entity.yaw;
-  while(true) {
-    t += 1;
-    const acceleration = new Vec3(0, 0, 0);
-    acceleration.x += bot.physics.walkingAcceleration * -Math.sin(yaw);
-    acceleration.z += bot.physics.walkingAcceleration * -Math.cos(yaw);
-    if (sprint) {
-      acceleration.x *= bot.physics.sprintSpeed
-      acceleration.z *= bot.physics.sprintSpeed
-    }
-    if (jump) vel.y = bot.physics.jumpSpeed;
-    acceleration.y -= bot.physics.gravity;
-    const oldGroundSpeedSquared = vel.x * vel.x + vel.z * vel.z;
-    if (oldGroundSpeedSquared < EPSILON) {
-      vel.x = 0
-      vel.z = 0
-    } else {
-      const oldGroundSpeed = Math.sqrt(oldGroundSpeedSquared)
-      let groundFriction = bot.physics.groundFriction * bot.physics.walkingAcceleration
-      if (!onGround) groundFriction *= 0.05
-      const maybeNewGroundFriction = oldGroundSpeed / 0.05;
-      groundFriction = groundFriction > maybeNewGroundFriction ? maybeNewGroundFriction : groundFriction;
-      acceleration.x -= vel.x / oldGroundSpeed * groundFriction;
-      acceleration.z -= vel.z / oldGroundSpeed * groundFriction;
-    }
-    vel.add(acceleration.scaled(0.05));
-    let currentMaxGroundSpeed;
-    const underBlock = bot.blockAt(pos.offset(0, -1, 0))
-    const inBlock = bot.blockAt(pos.offset(0, 0, 0))
-    if (underBlock && underBlock.type === 88) currentMaxGroundSpeed = bot.physics.maxGroundSpeedSoulSand
-    else if (inBlock && inBlock.type === 9) currentMaxGroundSpeed = bot.physics.maxGroundSpeedWater;
-    else currentMaxGroundSpeed = bot.physics.maxGroundSpeed;
-    if (sprint) currentMaxGroundSpeed *= bot.physics.sprintSpeed;
-    const groundSpeedSquared = vel.x * vel.x + vel.z * vel.z;
-    if (groundSpeedSquared > currentMaxGroundSpeed * currentMaxGroundSpeed) {
-      const groundSpeed = Math.sqrt(groundSpeedSquared)
-      const correctionScale = currentMaxGroundSpeed / groundSpeed
-      vel.x *= correctionScale
-      vel.z *= correctionScale
-    }
-    vel.y = Math.clamp(-bot.physics.terminalVelocity, vel.y, bot.physics.terminalVelocity)
-    console.log(vel);
-    let boundingBox = getBoundingBox(bot);
-    let boundingBoxMin;
-    let boundingBoxMax
-    if (vel.x !== 0) {
-      pos.x += vel.x * 0.05;
-      const blockX = Math.floor(pos.x + Math.sign(vel.x) * bot.physics.playerApothem)
-      boundingBoxMin = new Vec3(blockX, boundingBox.min.y, boundingBox.min.z)
-      boundingBoxMax = new Vec3(blockX, boundingBox.max.y, boundingBox.max.z)
-      if (collisionInRange(bot, boundingBoxMin, boundingBoxMax)) {
-        pos.x = blockX + (vel.x < 0 ? 1 + bot.physics.playerApothem : -bot.physics.playerApothem) * 1.001;
-        vel.x = 0;
-        boundingBox = getBoundingBox();
+  emulatePhysics(pos,yaw,forward,right,jump,onGround=true,sprint=true,vel=vec3(0,0,0)) {
+    onGround=onGround?onGround:bot.entity.onGround;
+    pos=pos?pos:bot.entity.position.clone();
+    yaw=yaw?yaw:bot.entity.yaw;
+    for(let t=1;t<=1;t++) {
+      // set acceleration to input
+      const acceleration = new Vec3(0, 0, 0);
+      if (forward || right) {
+        const rotationFromInput = Math.atan2(-right, forward)
+        const inputYaw = this.bot.entity.yaw + rotationFromInput
+        acceleration.x += this.bot.physics.walkingAcceleration * -Math.sin(inputYaw)
+        acceleration.z += this.bot.physics.walkingAcceleration * -Math.cos(inputYaw)
+        if (sprint) {
+          acceleration.x *= this.bot.physics.sprintSpeed
+          acceleration.z *= this.bot.physics.sprintSpeed
+        }
       }
-    }
-    if (vel.z !== 0) {
-      pos.z += vel.z * 0.05;
-      const blockZ = Math.floor(pos.z + Math.sign(vel.z) * bot.physics.playerApothem)
-      boundingBoxMin = new Vec3(boundingBox.min.x, boundingBox.min.y, blockZ)
-      boundingBoxMax = new Vec3(boundingBox.max.x, boundingBox.max.y, blockZ)
-      if (collisionInRange(bot, boundingBoxMin, boundingBoxMax)) {
-        pos.z = blockZ + (vel.z < 0 ? 1 + bot.physics.playerApothem : -bot.physics.playerApothem) * 1.001
+      acceleration.x += this.bot.physics.walkingAcceleration * -Math.sin(yaw);
+      acceleration.z += this.bot.physics.walkingAcceleration * -Math.cos(yaw);
+      if (sprint) {
+        acceleration.x *= this.bot.physics.sprintSpeed
+        acceleration.z *= this.bot.physics.sprintSpeed
+      }
+      if (jump&&onGround) vel.y = this.bot.physics.jumpSpeed;
+      acceleration.y -= this.bot.physics.gravity;
+      // ground friction
+      const oldGroundSpeedSquared = vel.x * vel.x + vel.z * vel.z;
+      if (oldGroundSpeedSquared < EPSILON) {
+        vel.x = 0
         vel.z = 0
-        boundingBox = getBoundingBox()
+      } else {
+        const oldGroundSpeed = Math.sqrt(oldGroundSpeedSquared)
+        let groundFriction = this.bot.physics.groundFriction * this.bot.physics.walkingAcceleration
+        if (!onGround) groundFriction *= 0.05
+        const maybeNewGroundFriction = oldGroundSpeed / 0.05;
+        groundFriction = groundFriction > maybeNewGroundFriction ? maybeNewGroundFriction : groundFriction;
+        acceleration.x -= vel.x / oldGroundSpeed * groundFriction;
+        acceleration.z -= vel.z / oldGroundSpeed * groundFriction;
+      }
+      // acceleration to velocity
+      vel.add(acceleration.scaled(0.05));
+      // change velocity based on ground
+      let currentMaxGroundSpeed;
+      const underBlock = this.blockAt(pos.offset(0, -1, 0))
+      const inBlock = this.blockAt(pos.offset(0, 0, 0))
+      if (underBlock && underBlock.type === 88) currentMaxGroundSpeed = this.bot.physics.maxGroundSpeedSoulSand
+      else if (inBlock && inBlock.type === 9) currentMaxGroundSpeed = this.bot.physics.maxGroundSpeedWater;
+      else currentMaxGroundSpeed = this.bot.physics.maxGroundSpeed;
+      if (sprint) currentMaxGroundSpeed *= this.bot.physics.sprintSpeed;
+      const groundSpeedSquared = vel.x * vel.x + vel.z * vel.z;
+      if (groundSpeedSquared > currentMaxGroundSpeed * currentMaxGroundSpeed) {
+        const groundSpeed = Math.sqrt(groundSpeedSquared)
+        const correctionScale = currentMaxGroundSpeed / groundSpeed
+        vel.x *= correctionScale
+        vel.z *= correctionScale
+      }
+      vel.y = Math.clamp(-bot.physics.terminalVelocity, vel.y, bot.physics.terminalVelocity)
+      // calculate new positions and resolve collisions
+      let boundingBox = this.getBoundingBox(pos);
+      let boundingBoxMin;
+      let boundingBoxMax;
+      if (vel.x !== 0) {
+        pos.x += vel.x * 0.05;
+        const blockX = Math.floor(pos.x + Math.sign(vel.x) * this.bot.physics.playerApothem)
+        boundingBoxMin = new Vec3(blockX, boundingBox.min.y, boundingBox.min.z)
+        boundingBoxMax = new Vec3(blockX, boundingBox.max.y, boundingBox.max.z)
+        if (this.collisionInRange(boundingBoxMin, boundingBoxMax)) {
+          pos.x = blockX + (vel.x < 0 ? 1 + this.bot.physics.playerApothem : -this.bot.physics.playerApothem) * 1.001;
+          vel.x = 0;
+          boundingBox = this.getBoundingBox(pos);
+        }
+      }
+      if (vel.z !== 0) {
+        pos.z += vel.z * 0.05;
+        const blockZ = Math.floor(pos.z + Math.sign(vel.z) * this.bot.physics.playerApothem)
+        boundingBoxMin = new Vec3(boundingBox.min.x, boundingBox.min.y, blockZ)
+        boundingBoxMax = new Vec3(boundingBox.max.x, boundingBox.max.y, blockZ)
+        if (this.collisionInRange(boundingBoxMin, boundingBoxMax)) {
+          pos.z = blockZ + (vel.z < 0 ? 1 + this.bot.physics.playerApothem : -this.bot.physics.playerApothem) * 1.001
+          vel.z = 0
+          boundingBox = this.getBoundingBox(pos)
+        }
+      }
+      onGround = false
+      if (vel.y !== 0) {
+        pos.y += vel.y * 0.05
+        const playerHalfHeight = bot.physics.playerHeight / 2
+        const blockY = Math.floor(pos.y + playerHalfHeight + Math.sign(vel.y) * playerHalfHeight)
+        boundingBoxMin = new Vec3(boundingBox.min.x, blockY, boundingBox.min.z)
+        boundingBoxMax = new Vec3(boundingBox.max.x, blockY, boundingBox.max.z)
+        if (this.collisionInRange(boundingBoxMin, boundingBoxMax)) {
+          pos.y = blockY + (vel.y < 0 ? 1 : -this.bot.physics.playerHeight) * 1.001
+          onGround = vel.y < 0;
+          vel.y = 0
+        }
       }
     }
-    onGround = false
-    if (vel.y !== 0) {
-      pos.y += vel.y * 0.05
-      const playerHalfHeight = bot.physics.playerHeight / 2
-      const blockY = Math.floor(pos.y + playerHalfHeight + Math.sign(vel.y) * playerHalfHeight)
-      boundingBoxMin = new Vec3(boundingBox.min.x, blockY, boundingBox.min.z)
-      boundingBoxMax = new Vec3(boundingBox.max.x, blockY, boundingBox.max.z)
-      if (collisionInRange(boundingBoxMin, boundingBoxMax)) {
-        pos.y = blockY + (vel.y < 0 ? 1 : -bot.physics.playerHeight) * 1.001
-        onGround = vel.y < 0 ? true : onGround
-        vel.y = 0
-      }
+    return {
+      pos: pos,
+      vel: vel,
+      onGround: onGround
     }
   }
+  radians=[
+    Math.PI*2,//0
+    Math.PI/4,//45
+    Math.PI/2,//90
+    3*Math.PI/4,//135
+    Math.PI,//180
+    5*Math.PI/4,//225
+    3*Math.PI/2,//270
+    7*Math.PI/4,//315
+  ]
+  getNeighborsUsingPhysics(node) {
+    const neighbors =[];
+    this.radians.forEach(r=>{
+
+    });
+    return result.map(el=>new Node(el.p, el.a));
+  }
 }
-function getNeighbors(node,bot) {
+/*function getNeighbors(node,bot) {
   const getBlock = p=>blockAt(bot,p);
   blocks={};
   const point = node.point
@@ -272,7 +288,7 @@ function getNeighbors(node,bot) {
     if(safePos(p)) result.push({p:p,a:'longjump'});
   }
   return result.map(el=>new Node(el.p, el.a));
-}
+}*/
 module.exports = {
   navigate: navigate
 }
